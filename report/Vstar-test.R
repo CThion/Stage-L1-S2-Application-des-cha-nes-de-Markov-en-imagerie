7430a3fb0d4c97e49267d6ad455a0372cf21de4e
Clément Thion <clement.thion@orange.fr>
2020-08-19 12:22:27 +0200

    comming back...

--- report/Vstar-test.R
+++ report/Vstar-test.R
@@ -1,29 +1,55 @@
 #========================================================================================================
-
-
-
-
 imageGenerator=function(forme, N){
-	#fonction permettant de générer une matrice-image de forme choisie, et de taille N*N
-	img <- matrix(-1, nrow= N, ncol= N)	
-	#----rectangle----
-	if(forme== "rectangle"){
-		img[(N/4):(N*3/4),(N/4):(N*3/4)] <- 1}
-	#----losange----
-	if(forme== "losange"){
-		for (i in 1:N){for (j in 1:N){
-				sigma0[i,j]=-1+2*(abs(i-j)<=N/4)*(abs(i+j-N)<=N/4)
-	}}}#if&for&for
-	#----damier----
-	#----triangle----
-	#----cercle----
-	#----bande----
-	if(forme== "bande"){
-		for (j in 1:(N/2)){ img[,2*j]=rep(1,N)}
-	}
-
-	return(img)
+  #fonction permettant de générer une matrice-image de forme choisie, et de taille N*N
+  
+  img <- matrix(-1, nrow= N, ncol= N)
+  
+  #----rectangle----
+  if(forme== "rectangle"){
+    img[(N/4):(N*3/4),(N/4):(N*3/4)] <- 1}
+  
+  #----losange----
+  if(forme== "losange"){
+    for (i in 1:N){
+      for (j in 1:N){
+        img[i,j]=-1+2*(abs(i-j)<=N/4)*(abs(i+j-N)<=N/4)  
+      }
+    }#for
+  }#if
+  
+  #----damier----
+  #----triangle----
+  #----cercle----
+  #----bande----
+  if(forme=="bande"){
+    for (j in 1:(N/2)){ img[,2*j]=rep(1,N) }
+  }#if
+  
+  
+  return(img)
 }#func
+
+
+
+#========================================================================================================
+bruiteur=function(image, br, p){
+  #fonction qui revoit l'image donnée en argument par la même image mais bruité par la méthode "br"
+  imgBr <- image
+  N <- dim(image)[1]
+  bruit <- 0
+  p <- 0.15
+  #----binomiale----
+  if(br== "rbinom"){bruit <- matrix(2*rbinom(N^2,1,1-p)-1,nrow=N,ncol=N)}
+  
+  #----poisson----
+  
+  #----normale----
+  
+  imgBr <- image*bruit
+  return(imgBr)
+}#func
+
+
 
 #========================================================================================================
 correspondance=function(img0, img1, N){
@@ -38,6 +64,8 @@ correspondance=function(img0, img1, N){
 	correspondance <- ((N^2)-nok)/(N^2)*100
 	return(correspondance)
 }#func
+
+
 
 #========================================================================================================
 vbranch=function(img, N, is, js, iv, jv, L){
@@ -69,13 +97,12 @@ vbranch=function(img, N, is, js, iv, jv, L){
 	#print(length(v))
 	return(length(v)*lead) #"poid" de la branche. length(v) fait office de facteur, comme beta classiquement
 }#func
-
-
 #vbranch(img, 50, 1,1, 0, -1, 50 )
+
+
+
 #========================================================================================================
-
-
-Vstar =function(img, N, i, j, L)a{
+Vstar =function(img, N, i, j, L){
 	s <- 0
 	s <- s + vbranch(img, N, i, j, -1, 0, L)
 	s <- s + vbranch(img, N, i, j, 1, 0, L)
@@ -85,21 +112,18 @@ Vstar =function(img, N, i, j, L)a{
 }#func
 
 
-#========================================================================================================
 
+#========================================================================================================
 metropolisIsing=function(forme, N, beta, B, alpha, L, n, p){
 	#fonction qui réalise l'algorithme de Metropolis sur une image img0 qui est bruité en img1, et doit à la fin être restaurée en img2
 	
 	#----création de l'image parfaite img0----
 	img0 = imageGenerator(forme, N)
-	image(img0)
 	
 	#----création d'une image auxiliaire img1, avec bord plus long et bruitage----
 	img1 = matrix(0, nrow=N+2, ncol=N+2)
-	bruit = matrix(2*rbinom(N^2,1,1-p)-1,nrow=N,ncol=N)
-	img1[2:(N+1),2:(N+1)] = img0*bruit
-	image(img1)
-	img2 = img1 #image tamponz
+	img1[2:(N+1),2:(N+1)] = bruiteur(img0, "rbinom", p)
+	img2 = img1 #image tampon
 	img3 = img1 #mémoire du bruit initiale
 
 	for(k in 1:n){
@@ -125,29 +149,209 @@ metropolisIsing=function(forme, N, beta, B, alpha, L, n, p){
 	
 		#----test----
 		dU = U2-U1
-		#if(dU<0){img1[i,j]= -img1[i,j]}
-		#else{
 		p <- exp(-U2)
+		
 		if(runif(1)<=p){img1[i,j]= -img1[i,j]}
 		else{img2[i,j] <- img1[i,j]} #sinon on ne change pas
-		#}#else
 	}#for
+	
 	#----affichage----
-	par(mfrow=c(1,3))
-	image(1:N,1:N,img0)
-	img3 <- img3[2:(N+1),2:(N+1)]
-	image(1:N+2,1:N+2,img3)
+	img3 <- img3[2:(N+1),2:(N+1)] #on retire les contours
 	img1 <- img1[2:(N+1),2:(N+1)]
-	image(1:N+2,1:N+2,img1)
-
-
-	#image(img1)
+	
+	par(mfrow=c(1,3))
+	image(1:N,1:N,img0)#image initiale
+	image(1:N+2,1:N+2,img3)#image bruitée
+	image(1:N+2,1:N+2,img1)#image restaurée
+  
+	#----observation des performance de restauration----
 	corresp <- correspondance(img0, img1, N)
-	return(corresp) #utile pour MCdeMC_Vstar
-	#calcul de la correspondance
+	return(corresp) 
 }#func
 
-#image(imageGenerator("bande",50))
+
 #metropolisIsing(forme, N, beta, B, alpha, L, n, p)
 metropolisIsing("bande", 64, 1, 0, 0.3, 5, 2*10^4, 0.15)
+
+
+
+#========================================================================================================
+recuitMetro=function(forme, N, beta, B, alpha, L, n, p){
+  #fonction qui réalise l'algorithme de Metropolis sur une image img0 qui est bruité en img1, et doit à la fin être restaurée en img2
+  
+  #----création de l'image parfaite img0----
+  img0 = imageGenerator(forme, N)
+  
+  #----création d'une image auxiliaire img1, avec bord plus long et bruitage----
+  img1 = matrix(0, nrow=N+2, ncol=N+2)
+  img1[2:(N+1),2:(N+1)] = bruiteur(img0, "rbinom", p)
+  img2 = img1 #image tampon
+  img3 = img1 #mémoire du bruit initiale
+  
+  for(k in 1:n){
+    
+    #---recuit des parametres----
+    t <- 1/log(k)
+    #Br <- B/t
+    #betar <- beta/t
+    alphar <- alpha/t    
+    
+    #----choix d'un site---
+    i <- 1+ceiling(N*runif(1)) # indice entre 2 et N+1
+    j <- 1+ceiling(N*runif(1))
+    #i <- 2 + k%%N #de 2 à N+1
+    #j <- 2 + k%/%(N-1)%%N #de 2 à N+1
+    
+    #----tirage nouvelle valeure candidate----
+    img2[i,j] = -img1[i,j]
+    
+    #----voisinage----
+    #v = img1[i+1,j] + img1[i-1,j] + img1[i,j+1] + img1[i,j-1]
+    v = Vstar(img1, N+2, i, j, L)
+    
+    #----potentiels----
+    #U1 = -B*img1[i,j]-beta*img1[i,j]*v-alpha*img3[i,j]
+    #U2 = -B*img2[i,j]-beta*img2[i,j]*v-alpha*img3[i,j]
+    #U1 = -Br*img1[i,j]-img1[i,j]*(betar*v + alphar*img3[i,j])#potentiel actuel
+    #U2 = -Br*img2[i,j]-img2[i,j]*(betar*v + alphar*img3[i,j])#potentiel candidat
+    U1 = -B/t*img1[i,j]-img1[i,j]*(beta/t*v + alpha/t*img3[i,j])#potentiel actuel
+    U2 = -B/t*img2[i,j]-img2[i,j]*(beta/t*v + alpha/t*img3[i,j])#potentiel candidat
+    
+    #----test----
+    dU = U2-U1
+    p <- exp(-U2)
+    
+    if(runif(1)<=p){img1[i,j]= -img1[i,j]}
+    else{img2[i,j] <- img1[i,j]} #sinon on ne change pas
+  }#for
+  
+  #----affichage----
+  img3 <- img3[2:(N+1),2:(N+1)] #on retire les contours
+  img1 <- img1[2:(N+1),2:(N+1)]
+  
+  par(mfrow=c(1,3))
+  image(1:N,1:N,img0)#image initiale
+  image(1:N+2,1:N+2,img3)#image bruitée
+  image(1:N+2,1:N+2,img1)#image restaurée
+  
+  #----observation des performance de restauration----
+  corresp <- correspondance(img0, img1, N)
+  return(corresp) 
+}#func
+
+recuitMetro("rectangle", 64, 1, 0, 0.3, 10^5, 0.15)
+
+
+
+#========================================================================================================
+recuitMetroHV=function(forme, N, p, alpha, n){
+  return(1)
+}#func
+
+
+
+
+#========================================================================================================
+recuitSimuleGuyader = function(n, beta, N, p, alpha, stop){
+  
+  #----création de sigma0 le rectangle initial----
+  sigma0 <- matrix(-1,nrow=N,ncol=N)  
+  sigma0[(N/4):(N*3/4),(N/4):(N*3/4)] <- 1
+  
+  #----création de sigma le rectangle bruité----
+  bruit <- matrix(2*rbinom(N^2,1,1-p)-1,nrow=N,ncol=N) 
+  sigma <- sigma0*bruit  
+  
+  #----création image auxiliaire, avec contour suplémentaire pour calculer U plus facilement----
+  Saux <- matrix(0, nrow=N+2, ncol=N+2) 
+  Saux[2:(N+1),2:(N+1)] <- sigma
+  sigmaaux <- Saux #sugmaaux = matrice auxiliaire pour le calcule de U, garde la valeur initiale de Saux (qui lui change)
+  
+  
+  #--------le recuit simulé--------
+  if(stop==0){ #cas où on s'arrête simplement après n itérations
+    for (k in (1:n)) {
+      #T <- 1/sqrt(sqrt((1+log(k))))
+      T <- 1/k #T tend lentement vers 0
+      iaux <- 1+ceiling(N*runif(1)) # indice entre 2 et N+1
+      jaux <- 1+ceiling(N*runif(1))
+      #iaux <- 2 + k%%N #de 2 à N+1
+      #jaux <- 2 + k%/%(N-1)%%N #de 2 à N+1
+      
+      
+      #----voisinage----
+      #s <- Vstar(Saux, N, iaux, jaux)
+      s <- Saux[iaux-1,jaux]+Saux[iaux+1,jaux]+Saux[iaux,jaux-1]+Saux[iaux,jaux+1]
+      #print("s")
+      #print(s)
+      #----proba pour un recuit simulé, avec rapport de métropolis----
+      r <- exp(-2*Saux[iaux,jaux]*(2*alpha*T*sigmaaux[iaux,jaux]+beta*s)/T)
+      
+      #----changement ou non de spin selon la proba r---
+      if(runif(1)<r){Saux[iaux,jaux] <- -Saux[iaux,jaux]}
+    }#for
+  }#if
+  
+  if(stop==1){#cas ou on s'arrête après n itérations sans changement de spin
+    X<-0#nombre d'itérations consécutives sans changement de spin
+    it<-0
+    while(X<=n) {
+      it<-it+1
+      #T <- 1/sqrt(sqrt((1+log(k))))
+      T <- 1/k #T tend lentement vers 0
+      iaux <- 1+ceiling(N*runif(1)) # indice entre 2 et N+1
+      jaux <- 1+ceiling(N*runif(1))
+      #iaux <- 2 + k%%N #de 2 à N+1
+      #jaux <- 2 + k%/%(N-1)%%N #de 2 à N+1
+      
+      
+      #----voisinage----
+      s <- Saux[iaux-1,jaux]+Saux[iaux+1,jaux]+Saux[iaux,jaux-1]+Saux[iaux,jaux+1]
+      
+      #----proba pour un recuit simulé----
+      r <- exp(-2*Saux[iaux,jaux]*(2*alpha*T*sigmaaux[iaux,jaux]+beta*s)/T)
+      
+      #----changement ou non de spin selon la proba r---
+      if(runif(1)<r){
+        Saux[iaux,jaux] <- -Saux[iaux,jaux]
+        X <- 0#on vient de changer un spin, donc la condition d'arrêt retombe à 0
+      }#if
+      else{X <-X+1 } 
+    }#while
+    print(it)
+  }#if
+  
+  S <- Saux[2:(N+1),2:(N+1)] #image finale (on retire le contour auxiliaire)
+  
+  
+  #----pourcentage de correspondance----
+  nok <- 0 #nombre de pixels divergents
+  correspondance <- 0 #pourcentage sur nb pixels total
+  for (i in (1:N)){
+    for (j in (1:N)){
+      if(S[i,j] != sigma0[i,j]){nok <- nok+1}
+    }#for
+  }#for
+  correspondance <- ((N^2)-nok)/(N^2)*100
+  #print(nok)
+  #print(correspondance)
+  
+  
+  #----affichage----
+  par(mfrow=c(1,3))
+  image(1:N,1:N,sigma0)
+  image(1:N,1:N,sigma)
+  image(1:N,1:N,S)
+  return(correspondance)
+}#function
+
+
+
+
+
+
+
+
+
+
 
